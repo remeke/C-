50df1427394028cfd92c30ffd8ac6a42964ed7ab
remeke <93020597+remeke@users.noreply.github.com>
2021-10-23 15:56:58 +0800

    Add files via upload

---
+++ 日常作业.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+int main()
+{
+ /*int a;
+  printf("输入一个整数:");
+scanf("%d",&a);
+if(a%2==0)
+   printf("%d是偶数\n",a);
+else
+   printf("%d是奇数\n",a);*/
+   //奇偶判断
+   
+   /*int i=1;
+   while(i<100)
+   {
+   	if(i%2==1)
+   	printf("%d\n",i);
+     i++;
+	} */
+	//输出1-100奇数
+	
+	/*int day=0;
+	scanf("%d",&day);
+switch(day)
+{
+case 1:
+	
+case 2:
+case 3:
+	case 4:
+	case 5:
+	printf("工作日\n");
+	break;
+	case 6:
+		case 7:
+	printf("休息日\n");
+	default:
+		printf("输入错误"); 
+}*/
+//switch语句
+
+
+/*int i=0;
+while(i<10)
+{
+i++;
+if(5==i)
+continue;
+printf("%d ",i);
+
+}*/
+int ch=0;
+while((ch=getchar() )!=EOF)
+{
+	putchar(ch);
+	}	
+return 0;
+}
+
